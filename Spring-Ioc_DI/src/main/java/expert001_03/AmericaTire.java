fc8cec00acb9bf0ac8a539041b9bfab96c1744aa
minkeun <minkeun428@gmail.com>
2021-05-31 13:17:44 +0900

    속성을 통한 의존성 주입

---
+++ Spring-Ioc_DI/src/main/java/expert001_03/AmericaTire.java
@@ -0,0 +1,9 @@
+package expert001_03;
+
+public class AmericaTire implements Tire {
+	
+	public String getBrand() {
+		return "코리아 타이어";
+	}
+
+}
---
+++ Spring-Ioc_DI/src/main/java/expert001_03/Tire.java
@@ -0,0 +1,7 @@
+package expert001_03;
+
+public interface Tire {
+
+	String getBrand();
+
+}
